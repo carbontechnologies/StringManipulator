fde331f91c1d41d588aa934461c9eb4cda8ea7e5
egntech <59536058+egntech@users.noreply.github.com>
2021-05-11 02:37:32 +0300

    Add project files.

---
+++ StringManipulator/StringManipulator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CarbonTechnologies
+{
+    public static class StringManipulator
+    {
+
+        /// <summary>
+        /// Finds the string in between.
+        /// Ex:
+        /// mainstring = "This is a bunch of instance words.",
+        /// firstring = "This is a",
+        /// secondstring = "instance",
+        /// RETURNS: " bunch of "
+        /// </summary>
+        /// <param name="mainstring">This is the string main string that will be searched</param>
+        /// <param name="firststring">First string (will be excluded from the returning string)</param>
+        /// <param name="secondstring">Second string (will be excluded from the returning string)</param>
+        /// <returns>Return the string between 1st and 2nd strings on mainstring</returns>
+        public static string FindStringBetween2Strings(string mainstring, string firststring, string secondstring)
+        {
+            int startingindex = mainstring.IndexOf(firststring) + firststring.Length;
+            int lastindex = mainstring.IndexOf(secondstring);
+
+            return mainstring.Substring(startingindex, lastindex - startingindex);
+                       
+        }
+
+
+    }
+}
---
+++ StringManipulatorTester/Form1.cs
@@ -0,0 +1,48 @@
+﻿using CarbonTechnologies;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringManipulatorTester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string teststring = StringManipulator.FindStringBetween2Strings(
+                "This is a bunch of instance words.",
+                "This is a",
+                "instance");
+
+            if (teststring == " bunch of ")
+                MessageBox.Show("Passed");
+            else
+                MessageBox.Show("Failed");
+
+
+            teststring = StringManipulator.FindStringBetween2Strings(
+                "123456789",
+                "234",
+                "78");
+
+            if (teststring == "56")
+                MessageBox.Show("Passed");
+            else
+                MessageBox.Show("Failed");
+
+
+
+        }
+    }
+}
